d7f616ddd086d1aaab80f695d3abf8f3d19453d6
Marcel Savegnago <marcel.savegnago@gmail.com>
2020-06-30 12:16:50 -0300

    [12.0][IMP] maintenance_request_report

---
+++ maintenance_request_report/__manifest__.py
@@ -0,0 +1,20 @@
+# Copyright 2020 - TODAY, Escodoo
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
+
+{
+    'name': 'Maintenance Request Report',
+    'summary': """
+        Maintenance Request Report""",
+    'version': '12.0.1.0.0',
+    'license': 'AGPL-3',
+    'author': 'Escodoo,Odoo Community Association (OCA)',
+    'website': 'www.escodoo.com.br',
+    'depends': [
+        'maintenance',
+    ],
+    'data': [
+        'views/report_maintenance_request.xml',
+    ],
+    'demo': [
+    ],
+}
